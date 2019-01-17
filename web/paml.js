30e88293f249a4351427b1d661a58bfa5a3efa0d
Joe DF <joedf@live.ca>
2019-01-17 11:13:45 -0500

    Update paml.js

--- web/paml.js
+++ web/paml.js
@@ -5,3 +5,28 @@ var paml = doc.getElementsByTagName('paml')[0];
 paml.getElementsByTagName('info')[0].textContent.replace(/[ \t]/ig,'').trim().split('\n');
 paml.getElementsByTagName('defcolor')[0].textContent.replace(/[ \t]/ig,'').trim().split('\n');
 paml.getElementsByTagName('drawpixels')[0].textContent.replace(/[ \t]/ig,'').trim().split('\n');
+
+
+rawPaml = " , , ,b,y,r,...";
+
+uniqueDefs = '';
+newPixelData = '';
+colorlist = [];
+
+/* //pseudo code...
+for each def in rawPaml
+    make string like this: var b='color:#000;background:#000';
+    colorlist.push('b');
+
+for each line in rawPaml pixelData
+    for each color in colorlist
+        uniqueDefs += regexReplace(line, '('+color+'\,)+', color+',');
+    add a %c before each new def symbol change
+    add \n at end of line
+    if doublewidth
+        double each def symbol
+    append modified line to newPixelData
+
+finally create the js call
+    console.log(newPixelData, the defs: b,g,t,b,g,b,....);
+/*
